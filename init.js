d9732733ef079b42ff5c6fb21d40b090f99f4dfd
Andrey Poznyak <andpoznyak@gmail.com>
2014-03-12 18:28:04 +0300

    work with db structure started

--- init.js
+++ init.js
@@ -13,14 +13,44 @@ sequelize.authenticate().complete(function(err) {
     }
 });
 
-var Test = sequelize.define('Test', {
-  field: Sequelize.STRING
-});
+defineTables();
 
 sequelize.sync({ force: true }).complete(function(err) {
 	if (err) {
-		console.log('An error occurred while create the table:', err)
+		console.log('An error occurred while creating the tables:', err)
 	} else {
-		console.log('It worked!')
+		console.log('All tables created!')
 	}
 });
+
+function defineTables() {
+	//tables creation
+	var Coordinate = sequelize.define('Coordinate', {
+		longitude: Sequelize.FLOAT,
+		latitude: Sequelize.FLOAT,
+		strength: Sequelize.INTEGER
+	}, {
+		timestamps: false
+	});
+
+	var Hotspot = sequelize.define('Hotspot', {
+		name: Sequelize.STRING,
+		longitude: Sequelize.FLOAT,
+		latitude: Sequelize.FLOAT,
+		determined: Sequelize.BOOLEAN,
+		password: Sequelize.STRING
+	}, {
+		timestamps: false
+	});
+
+	var User = sequelize.define('User', {
+		name: Sequelize.STRING
+	});
+	
+	//one-to-many relations
+	Hotspot.hasMany(Coordinate);
+	Coordinate.belongsTo(Hotspot);
+
+	User.hasMany(Hotspot);
+	Hotspot.belongsTo(User);
+};
